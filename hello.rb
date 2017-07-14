2ec35500d84691497ef3ae3fc00b4505143d6b9f
kusakabe <gworks@kusakabenoMacBook-Pro.local>
2017-07-14 13:10:19 +0900

    helper do

--- hello.rb
+++ hello.rb
@@ -12,6 +12,14 @@ after do
     logger.info "page displayed successfully"
 end
 
+helpers do
+
+    def strong(s)
+        "<strong>#{s}</strong>"
+    end
+
+end
+
 get '/' do
     @title = "main index"
     @content = "main content by " + @author
@@ -20,8 +28,8 @@ end
 
 get '/about' do
     @title = "about this page"
-    @content = "this page is ... by " + @author
-    @email = "taguchi@gmail.com"
+    @content = "this page is ... by " + strong(@author)
+    @email = "kusakabe@gworks.jp"
     erb :about
 end
 
